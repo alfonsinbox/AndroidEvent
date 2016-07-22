e97f7d8b86e77a96ba186eb6c1903947d9bdcc2c
Alfons Lange <alfons_lange97@hotmail.com>
2016-07-22 18:44:36 +0200

    small bug fix with notification register

--- EventTest/app/src/main/java/com/example/alfon/eventtest/NotificationUtilities.java
+++ EventTest/app/src/main/java/com/example/alfon/eventtest/NotificationUtilities.java
@@ -1,7 +1,10 @@
 package com.example.alfon.eventtest;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.util.Pair;
 
@@ -16,31 +19,36 @@ import java.util.List;
  * Created by alfon on 2016-04-15.
  */
 public class NotificationUtilities {
-    public static void registerGcmId(final Context activity, final String gcmRegistrationId, MobileServiceClient mClient, final ServiceFilterResponseCallback serviceFilterResponseCallback) {
-        TokenCheck tokenCheck = new TokenCheck(activity, mClient) {
+    public static void registerGcmId(final Context activity, final String gcmRegistrationId, final MobileServiceClient mClient, final ServiceFilterResponseCallback serviceFilterResponseCallback) {
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
             @Override
-            public void onSuccess() {
+            public void onReceive(Context context, Intent intent) {
+                activity.unregisterReceiver(this);
+
                 List<Pair<String, String>> params = new ArrayList<>();
                 params.add(Pair.create("gcm_registration_id", gcmRegistrationId));
+                params.add(Pair.create("refresh_token", AuthUtilities.getLocalToken(GlobalApplication.PREFERENCE_USER_REFRESH_TOKEN, activity)));
 
                 List<Pair<String, String>> headers = new ArrayList<>();
-                headers.add(Pair.create("X-ZUMO-AUTH", AuthUtilities.getLocalToken(GlobalApplication.PREFERENCE_USER_REFRESH_TOKEN, activity)));
+                headers.add(Pair.create("X-ZUMO-AUTH", AuthUtilities.getLocalToken(GlobalApplication.PREFERENCE_USER_ACCESS_TOKEN, activity)));
 
                 mClient.invokeApi("notification/register/google", null, "POST", headers, params, serviceFilterResponseCallback);
             }
-
-            @Override
-            public void onFailure(Exception exception) {
-
-            }
         };
-        tokenCheck.checkToken();
+        IntentFilter intentFilter = new IntentFilter(GlobalApplication.ACTION_TOKEN_CHECK_CALLBACK_SUCCESS);
+        activity.registerReceiver(receiver, intentFilter);
+        Intent serviceIntent = new Intent(activity, TokenCheckService.class);
+        activity.startService(serviceIntent);
     }
 
     public static void deleteRegistration(final Context activity, final String registrationId, final MobileServiceClient mClient, final ServiceFilterResponseCallback serviceFilterResponseCallback){
-        TokenCheck tokenCheck = new TokenCheck(activity, mClient) {
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
             @Override
-            public void onSuccess() {
+            public void onReceive(Context context, Intent intent) {
+                activity.unregisterReceiver(this);
+
                 List<Pair<String, String>> params = new ArrayList<>();
                 params.add(Pair.create("registration_id", registrationId));
 
@@ -49,20 +57,18 @@ public class NotificationUtilities {
 
                 mClient.invokeApi("notification/delete", null, "POST", headers, params, serviceFilterResponseCallback);
             }
-
-            @Override
-            public void onFailure(Exception exception) {
-
-            }
         };
-        tokenCheck.checkToken();
+        IntentFilter intentFilter = new IntentFilter(GlobalApplication.ACTION_TOKEN_CHECK_CALLBACK_SUCCESS);
+        activity.registerReceiver(receiver, intentFilter);
+        Intent serviceIntent = new Intent(activity, TokenCheckService.class);
+        activity.startService(serviceIntent);
     }
 
     public static void setDeviceRegisteredForNotifications(Context activity, boolean valueToPut){
         SharedPreferences storedUserPreferences = activity.getSharedPreferences(GlobalApplication.PREFERENCES_USERSETTINGS, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = storedUserPreferences.edit();
         editor.putBoolean(GlobalApplication.PREFERENCE_DEVICE_REGISTERED_FOR_PUSH, valueToPut);
-        editor.commit();
+        editor.apply();
     }
     public static boolean isDeviceRegistered(Context activity){
         return activity
@@ -74,7 +80,7 @@ public class NotificationUtilities {
         SharedPreferences storedUserPreferences = activity.getSharedPreferences(GlobalApplication.PREFERENCES_USERSETTINGS, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = storedUserPreferences.edit();
         editor.putString(GlobalApplication.PREFERENCE_REGISTRATION_ID, registrationId);
-        editor.commit();
+        editor.apply();
     }
     public static String getRegistrationId(Context activity){
         return activity
